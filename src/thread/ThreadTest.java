9f721e82f161c24d8f838018d1af74e4dc918cf7
jinee <sksowk156@gmail.com>
2023-09-07 19:23:12 +0900

    230907 thread

--- src/thread/ThreadTest.java
+++ src/thread/ThreadTest.java
@@ -42,6 +42,9 @@ package thread;
 
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 //class Thread1 extends Thread{
 //    @Override
@@ -58,67 +61,348 @@ import javax.swing.*;
 //        System.out.println("카운트가 종료됨");
 //    }
 //}
-class ThreadRunnable implements Runnable {
-    Thread th; // 내부에서 사용할 Thread 객체를 하나 생성
+//class ThreadRunnable implements Runnable {
+//    Thread th; // 내부에서 사용할 Thread 객체를 하나 생성
+//
+//    @Override
+//    public void run() {
+//        String name = th.getName();
+//        while (!stopped) {
+//            if (!suspended) {
+//                try {
+//                    System.out.println(name+" 작업중");
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    System.out.println(name + " interrupt()로 인해 예외상황 발생");
+//                }
+//            } else {
+//                Thread.yield();
+//            }
+//        }
+//        System.out.println(name + "쓰레드 종료");
+//    }
+//
+//    boolean suspended = false; // suspend()와 stop()을 대체하기 위해
+//    boolean stopped = false;
+//
+//    ThreadRunnable(String name) { // 생성자를 통해서 내부에서 쓰레드를 생성해 쓰자
+//        th = new Thread(this, name); // Thread(Runnable r, String name)
+//    }
+//
+//    public void suspend() {
+//        suspended = true;
+//        th.interrupt();
+//        System.out.println(th.getName() + " suspend가 interrupt()호출 -> while문 동작O, if문 동작X, else문 동작");
+//    }
+//
+//    public void stop() {
+//        stopped = true;
+//        th.interrupt();
+//        System.out.println(th.getName() + " stop이 interrupt()호출 -> while문 동작X");
+//    }
+//
+//    public void resume() {
+//        suspended = true;
+//    }
+//
+//    public void start() {
+//        th.start();
+//    }
+//
+//    public void join(){
+//        try {
+//            th.join();
+//        } catch (InterruptedException e) {}
+//    }
+//}
+//class Account {
+//    private int balance = 1000;
+//
+//    public int getBalance() {
+//        return balance;
+//    }
+//
+//    public synchronized void withdraw(int money) {
+//        if (balance >= money) {
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//            }
+//            balance -= money;
+//        }
+//    }
+////    public void withdraw(int money) {
+////        synchronized (this) {
+////            if (balance >= money) {
+////                try {
+////                    Thread.sleep(1000);
+////                } catch (InterruptedException e) {
+////                }
+////                balance -= money;
+////            }
+////        }
+////    }
+//}
+//
+//class ThreadSample implements Runnable {
+//    Account acc = new Account();
+//
+//    @Override
+//    public void run() {
+//        while (acc.getBalance() > 0) {
+//            int money = (int) (Math.random() * 3 + 1) * 100;
+//            acc.withdraw(money);
+//            System.out.println("balance : " + acc.getBalance());
+//        }
+//    }
+//}
+//class Customer implements Runnable {
+//    private Table table;
+//    private String food;
+//
+//    Customer(Table table, String food) {
+//        this.table = table;
+//        this.food = food;
+//    }
+//
+////    boolean eatFood() {
+////        return table.remove(food);
+////    }
+//
+//    @Override
+//    public void run() {
+//        while (true) {
+//            try {
+//                Thread.sleep(10);
+//            } catch (InterruptedException e) {
+//            }
+//
+//            String name = Thread.currentThread().getName();
+//            table.remove(food);
+//            System.out.println(name + " ate a " + food);
+//
+////            if (eatFood()) {
+////                System.out.println(name + " ate a " + food);
+////            } else {
+////                System.out.println(name + " failed to eat :");
+////            }
+//        }
+//    }
+//}
+//
+//class Cook implements Runnable {
+//    private Table table;
+//
+//    Cook(Table table) {
+//        this.table = table;
+//    }
+//
+//    @Override
+//    public void run() {
+//        while (true) {
+//            int idx = (int) (Math.random() * table.dishNum());
+//            table.add(table.dishNames[idx]);
+//            try {
+//                Thread.sleep(100);
+//            } catch (InterruptedException e) {
+//            }
+//        }
+//    }
+//}
+//
+//class Table {
+//    // Cook객체가 요리를 만들 때 이 배열에서 랜덤으로 뽑아 요리한다. -> donut 확률을 높이기 위해 donut이 2개
+//    String[] dishNames = {"donut", "donut", "burger"};
+//    final int MAX_FOOD = 6;
+//    private ArrayList<String> dishes = new ArrayList<>();
+//
+//    public int dishNum() {
+//        return dishNames.length;
+//    }
+//
+//    public synchronized void add(String dish) {
+////        if (dishes.size() >= MAX_FOOD) {
+////            return;
+////        }
+//        while(dishes.size() >= MAX_FOOD){
+//            String name = Thread.currentThread().getName();
+//            System.out.println(name + " 테이블이 꽉차서 요리사가 기다리는중");
+//            try {
+//                wait(); // 요리사 쓰레드를 기다리게 한다.
+//                Thread.sleep(1000); // 1초간 기다림
+//            } catch (InterruptedException e) {}
+//        }
+//        dishes.add(dish); // 음식 완성
+//        notify(); // 기다리고 있던 손님 쓰레드를 깨운다.
+//        System.out.println("Dishes : " + dishes.toString());
+//    }
+//
+//    public void remove(String dishName) {
+//        synchronized (this) {
+//            String name = Thread.currentThread().getName();
+//            System.out.println(name + " 먹을려고 앉았다");
+//            while (dishes.size() == 0) {
+//                System.out.println(name + " 음식이 없어서 기다리는중");
+//                try {
+//                    wait(); // 손님 쓰레드를 기다리게 한다.
+//                    Thread.sleep(500);
+//                } catch (InterruptedException e) {}
+//            }
+//            while(true){
+//                for (int i = 0; i < dishes.size(); i++) {
+//                    if (dishName.equals(dishes.get(i))) {
+//                        System.out.println(name + " 식사 끝");
+//                        dishes.remove(i);
+//                        notify(); // 기다리고 있던 요리사 쓰레드를 깨운다.
+//                    }
+//                }
+//                try {
+//                    System.out.println(name + " 원하는 음식이 없어서 기다리는중");
+//                    wait();
+//                    Thread.sleep(500);
+//                } catch (InterruptedException e) {}
+//            }
+//
+////            if(dishes.size() > 0){
+////                for (int i = 0; i < dishes.size(); i++) {
+////                    if (dishName.equals(dishes.get(i))) {
+////                        System.out.println(name + " 식사 끝");
+////                        dishes.remove(i);
+////                        return true;
+////                    }
+////                }
+////                System.out.println(name + " 원하는게 없어 못먹음");
+////            }
+//        }
+//    }
+//}
+
+class Customer implements Runnable {
+    private Table table;
+    private String food;
+
+    Customer(Table table, String food) {
+        this.table = table;
+        this.food = food;
+    }
 
     @Override
     public void run() {
-        String name = th.getName();
-        while (!stopped) {
-            if (!suspended) {
-                try {
-                    System.out.println(name+" 작업중");
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    System.out.println(name + " interrupt()로 인해 예외상황 발생");
-                }
-            } else {
-                Thread.yield();
+        while (true) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
             }
+            String name = Thread.currentThread().getName();
+            table.remove(food);
+            System.out.println(name + " ate a " + food);
         }
-        System.out.println(name + "쓰레드 종료");
-    }
-
-    boolean suspended = false; // suspend()와 stop()을 대체하기 위해
-    boolean stopped = false;
-
-    ThreadRunnable(String name) { // 생성자를 통해서 내부에서 쓰레드를 생성해 쓰자
-        th = new Thread(this, name); // Thread(Runnable r, String name)
-    }
-
-    public void suspend() {
-        suspended = true;
-        th.interrupt();
-        System.out.println(th.getName() + " suspend가 interrupt()호출 -> while문 동작O, if문 동작X, else문 동작");
-    }
-
-    public void stop() {
-        stopped = true;
-        th.interrupt();
-        System.out.println(th.getName() + " stop이 interrupt()호출 -> while문 동작X");
-    }
-
-    public void resume() {
-        suspended = true;
-    }
-
-    public void start() {
-        th.start();
-    }
-
-    public void join(){
-        try {
-            th.join();
-        } catch (InterruptedException e) {}
     }
 }
 
+class Cook implements Runnable {
+    private Table table;
+
+    Cook(Table table) {
+        this.table = table;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            int idx = (int) (Math.random() * table.dishNum());
+            table.add(table.dishNames[idx]);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+}
+
+class Table {
+    // Cook객체가 요리를 만들 때 이 배열에서 랜덤으로 뽑아 요리한다. -> donut 확률을 높이기 위해 donut이 2개
+    String[] dishNames = {"donut", "donut", "burger"};
+    final int MAX_FOOD = 6;
+    private ArrayList<String> dishes = new ArrayList<>();
+
+    private ReentrantLock lock = new ReentrantLock(); // 공유 대상이 되는 클래스 내부에 생성
+    private Condition forCook = lock.newCondition();
+    private Condition forCust = lock.newCondition();
+
+    public int dishNum() {
+        return dishNames.length;
+    }
+
+    public void add(String dish) {
+        lock.lock();
+        try {
+            while (dishes.size() >= MAX_FOOD) {
+                String name = Thread.currentThread().getName();
+                System.out.println(name + " 기다리는 중");
+                try {
+                    forCook.await(); // wait(), Cook쓰레드를 기다리게 한다.
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+            dishes.add(dish);
+            forCust.signal(); // notify(), 기다리고 있는 Cust쓰레드를 깨운다.
+            System.out.println("Dishes : " + dishes.toString());
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void remove(String dishName) {
+        lock.lock();
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " 먹을려고 앉았다");
+        try {
+            while (dishes.size() == 0) {
+                System.out.println(name + " 음식이 없어서 기다리는중");
+                try {
+                    forCust.await(); // wait(), Cust쓰레드를 기다리게 한다.
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+            while (true) {
+                for (int i = 0; i < dishes.size(); i++) {
+                    if (dishName.equals(dishes.get(i))) {
+                        System.out.println(name + " 식사 끝");
+                        dishes.remove(i);
+                        forCook.signal(); // notify(), 기다리고 있던 요리사 쓰레드를 깨운다.
+                    }
+                }
+                try {
+                    System.out.println(name + " 원하는 음식이 없어서 기다리는중");
+                    forCust.await(); // wait(), Cust쓰레드를 기다리게 한다.
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+
 public class ThreadTest {
     public static void main(String[] args) {
-        Thread temp = new Thread();
-        try {
-            temp.join();
-        } catch (InterruptedException e) {}
+        Table table = new Table(); // 여러 쓰레드가 공유하는 객체
+        new Thread(new Cook(table), "Cook1").start();
+        new Thread(new Customer(table, "donut"), "Cust1").start();
+        new Thread(new Customer(table, "burger"), "Cust2").start();
+//        Runnable r = new ThreadSample();
+//        new Thread(r).start();
+//        new Thread(r).start();
+
+//        Thread temp = new Thread();
+//        try {
+//            temp.join();
+//        } catch (InterruptedException e) {}
 
 //        ThreadRunnable temp1 = new ThreadRunnable("1");
 //        ThreadRunnable temp2 = new ThreadRunnable("2");
