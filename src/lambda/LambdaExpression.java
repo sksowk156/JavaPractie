790afca55812ad8981a2502ecf74f94b880ccf3c
jinee <sksowk156@gmail.com>
2023-09-23 17:49:55 +0900

    Lanbda finish

--- src/lambda/LambdaExpression.java
+++ src/lambda/LambdaExpression.java
@@ -4,52 +4,87 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Objects;
 import java.util.SimpleTimeZone;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
-@FunctionalInterface
-interface MyFunc {
-    //    public abstract int method(int a, int b);
-    void run();
-}
-
-class Out {
-    int outVal = 10;
-
-    class In {
-        int inVal = 20;
-
-        void inMethod(int temp) {
-            int inMethodVal = 30;
-            MyFunc test = () -> {
-                System.out.println(++outVal); // 11
-                System.out.println(++inVal); // 21
-                System.out.println(temp); // 40
-                System.out.println(inMethodVal); // 30
-            };
-            test.run();
-        }
-    }
-}
-
+//@FunctionalInterface
+//interface MyFunc {
+//    //    public abstract int method(int a, int b);
+//    void run();
+//}
+//
+//class Out {
+//    int outVal = 10;
+//
+//    class In {
+//        int inVal = 20;
+//
+//        void inMethod(int temp) {
+//            int inMethodVal = 30;
+//            MyFunc test = () -> {
+//                System.out.println(++outVal); // 11
+//                System.out.println(++inVal); // 21
+//                System.out.println(temp); // 40
+//                System.out.println(inMethodVal); // 30
+//            };
+//            test.run();
+//        }
+//    }
+//}
+//
+//class Myclass{
+//    Boolean test(Myclass a){
+//        return true;
+//    }
+//}
 public class LambdaExpression {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            list.add(i);
-        }
-        HashMap<String, String> map = new HashMap<>();
-        map.put("1", "1");
-        map.put("2", "2");
-        map.put("3", "3");
+//        Function<String, Integer> a = (String s) -> Integer.parseInt(s);
+//        Function<String, Integer> b = Integer::parseInt;
+//        System.out.println(a.apply("1"));
+//        System.out.println(b.apply("1"));
+//
+//        Myclass temp = new Myclass();
+//        Function<Myclass, Boolean> c = temp::test;
+////        Function<Myclass, Boolean> d = Myclass::test;
+//        BiFunction<String, String, Boolean> f = String::equals;
 
-        list.forEach(i -> System.out.println(i));
-        System.out.println();
-        list.removeIf(x -> x % 2 == 0 || x % 3 == 0);
-        System.out.println(list);
-        list.replaceAll(i -> i * 10); // list의 각요소 곱하기 10
-        System.out.println(list);
-        map.forEach((k, v) -> System.out.println(k + " " + v));
-        System.out.println();
+//        Predicate<Integer> a = i -> i < 100;
+//        Predicate<Integer> b = i -> i < 200;
+//        Predicate<Integer> c = i -> i % 2 == 0;
+//
+//        Predicate<Integer> notA = a.negate();
+//        Predicate<Integer> allMethod = notA.and(b).or(c); // !false && true || false
+//        System.out.println(allMethod.test(150)); // true
+//
+//        Predicate<String> d = Predicate.isEqual("abc");
+//        boolean result = d.test("abc");
+//        System.out.println(result); // true
+
+//        Function<String, Integer> a = (s) -> Integer.parseInt(s, 16);
+//        Function<Integer, String> b = (i) -> Integer.toBinaryString(i);
+//        Function<String, String> c = a.andThen(b);
+//        System.out.println(c.apply("FF")); // 11111111
+//        Function<Integer, Integer> d = a.compose(b);
+//        System.out.println(d.apply(2)); // 16
+//        ArrayList<Integer> list = new ArrayList<>();
+//        for (int i = 0; i < 10; i++) {
+//            list.add(i);
+//        }
+//        HashMap<String, String> map = new HashMap<>();
+//        map.put("1", "1");
+//        map.put("2", "2");
+//        map.put("3", "3");
+//
+//        list.forEach(i -> System.out.println(i));
+//        System.out.println();
+//        list.removeIf(x -> x % 2 == 0 || x % 3 == 0);
+//        System.out.println(list);
+//        list.replaceAll(i -> i * 10); // list의 각요소 곱하기 10
+//        System.out.println(list);
+//        map.forEach((k, v) -> System.out.println(k + " " + v));
+//        System.out.println();
 
 //        Predicate<String> forCondition = s -> s.length() == 0;
 //        if(!forCondition.test("Predicate인터페이스의 test메서드")){
